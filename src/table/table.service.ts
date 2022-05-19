67feab73caaf5e1d6d7d5bc7db66540652fd9047
mlucasdev <lucasmsantos819@gmail.com>
2022-05-19 16:46:17 -0300

    feat(table): create 'TableService'

---
+++ src/table/table.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTableDto } from './dto/create-table.dto';
+import { Table } from './entities/table.entity';
+
+@Injectable()
+export class TableService {
+  tables: Table[] = [];
+  findAll() {
+    return;
+  }
+  create(createTableDto: CreateTableDto) {
+    const table: Table = { id: 'random_id', ...createTableDto };
+    this.tables.push(table);
+    return table;
+  }
+}
