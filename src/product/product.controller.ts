967f1551e5257408971a23dd4bdabdc5a7a47d34
mlucasdev <lucasmsantos819@gmail.com>
2022-06-02 18:38:40 -0300

    feat(product): implement `create`

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { CreateProductDto } from "./dto/create-product.dto";
+import { Product } from "./entities/product.entity";
+import { ProductService } from "./product.service";
 
 @Controller('product')
 export class ProductController {
-  
+  constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  create(@Body() dto: CreateProductDto): Promise<Product> {
+    return this.productService.create(dto);
+  }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,6 +1,14 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductService {
-  
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(dto: CreateProductDto): Promise<Product> {
+    const data: Product = { ...dto };
+    return this.prisma.product.create({ data });
+  }
 }
