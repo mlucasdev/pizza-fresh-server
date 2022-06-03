2816dd0fe6ea38e6ff823e82523e9317447bdb1d
mlucasdev <lucasmsantos819@gmail.com>
2022-06-03 16:54:35 -0300

    feat(user): implement `delete`

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -40,5 +48,13 @@ export class UserController {
   })
   update(@Param('id') id: string, @Body() dto: UpdateUserDto): Promise<User> {
     return this.userService.update(id, dto);
+  }
+
+  @Delete(':id')
+  @ApiOperation({
+    summary: 'Deletar um Usuário pelo Id',
+  })
+  delete(@Param('id') id: string) {
+    return this.userService.delete(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UpdateProductDto } from 'src/product/dto/update-product.dto';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -25,5 +25,10 @@ export class UserService {
   update(id: string, dto: UpdateProductDto): Promise<User> {
     const data: Partial<User> = { ...dto };
     return this.prisma.user.update({ where: { id }, data });
+  }
+
+  async delete(id: string) {
+    await this.prisma.user.delete({ where: { id } });
+    throw new HttpException('', 204);
   }
 }
