99ce9fdbb235e378859a2081da32a2abbd57992f
mlucasdev <lucasmsantos819@gmail.com>
2022-06-03 15:50:04 -0300

    feat(user): implement `create`

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,1 +1,13 @@
-export class UserController {}
+import { Body, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import { UserService } from './user.service';
+
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() dto: CreateUserDto): Promise<User> {
+    return this.userService.create(dto);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,1 +1,13 @@
-export class UserService {}
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(dto: CreateUserDto): Promise<User> {
+    delete dto.confirmPassword;
+    const data: User = { ...dto };
+    return this.prisma.user.create({ data });
+  }
+}
