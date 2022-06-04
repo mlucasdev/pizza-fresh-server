e47dcee4d7c9ff3999eb8de9aabe4daf2fac4936
mlucasdev <lucasmsantos819@gmail.com>
2022-06-03 22:19:24 -0300

    feat(product): implement `findById`

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { handleError } from 'src/utils/handle-error.util';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -19,16 +19,30 @@ export class ProductService {
   }
 
   findOne(id: string): Promise<Product> {
-    return this.prisma.product.findUnique({ where: { id } });
+    return this.findById(id);
   }
 
-  update(id: string, dto: UpdateProductDto): Promise<Product> {
+  async update(id: string, dto: UpdateProductDto): Promise<Product> {
+    await this.findById(id);
+
     const data: Partial<Product> = { ...dto };
-    return this.prisma.product.update({ where: { id }, data }).catch(handleError);
+    return this.prisma.product
+      .update({ where: { id }, data })
+      .catch(handleError);
   }
 
   async delete(id: string) {
+    await this.findById(id);
+    
     await this.prisma.product.delete({ where: { id } });
     throw new HttpException('', 204);
+  }
+
+  async findById(id: string): Promise<Product> {
+    const record = await this.prisma.product.findUnique({ where: { id } });
+    if (!record) {
+      throw new NotFoundException(`Registro com ID '${id}' não encontrado.`);
+    }
+    return record;
   }
 }
