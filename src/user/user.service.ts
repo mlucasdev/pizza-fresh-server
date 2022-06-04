aa0b56f2154b1adc6c6c79ea2b2d5378379433cc
mlucasdev <lucasmsantos819@gmail.com>
2022-06-03 21:46:35 -0300

    feat(user): implement `findById` and `handleError`

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,7 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UpdateProductDto } from 'src/product/dto/update-product.dto';
+import { handleError } from 'src/utils/handle-error.util';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 
@@ -11,7 +12,7 @@ export class UserService {
   create(dto: CreateUserDto): Promise<User> {
     delete dto.confirmPassword;
     const data: User = { ...dto };
-    return this.prisma.user.create({ data });
+    return this.prisma.user.create({ data }).catch(handleError);
   }
 
   findAll(): Promise<User[]> {
@@ -19,16 +20,28 @@ export class UserService {
   }
 
   findOne(id: string): Promise<User> {
-    return this.prisma.user.findUnique({ where: { id } });
+    return this.findById(id);
   }
 
-  update(id: string, dto: UpdateProductDto): Promise<User> {
+  async update(id: string, dto: UpdateProductDto): Promise<User> {
+    await this.findById(id);
     const data: Partial<User> = { ...dto };
-    return this.prisma.user.update({ where: { id }, data });
+    return this.prisma.user.update({ where: { id }, data }).catch(handleError);
   }
 
   async delete(id: string) {
+    await this.findById(id);
     await this.prisma.user.delete({ where: { id } });
     throw new HttpException('', 204);
+  }
+
+  async findById(id: string): Promise<User> {
+    const record = await this.prisma.user.findUnique({ where: { id } });
+
+    if (!record) {
+      throw new NotFoundException(`Registro com ID '${id}' não encontrado.`);
+    }
+
+    return record;
   }
 }
