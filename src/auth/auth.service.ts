34a7e1d5fd649ecd420558f6adeae8101b2a7ad9
mlucasdev <lucasmsantos819@gmail.com>
2022-06-12 17:01:14 -0300

    modified(login): change example nickname

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginResponseDto } from './dto/login-response.dto';
 import { LoginDto } from './dto/login.dtp';
@@ -8,9 +9,25 @@ export class AuthSercive {
   constructor(private readonly prisma: PrismaService) {}
 
   async login(dto: LoginDto): Promise<LoginResponseDto> {
+    const { nickname, password } = dto;
+
+    const user = await this.prisma.user.findUnique({ where: { nickname } });
+
+    if (!user) {
+      throw new UnauthorizedException('Usuário e/ou senha inválidos');
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      throw new UnauthorizedException('Usuário eee/ou senha inválidos');
+    }
+
+    delete user.password;
+
     return {
       token: 'Teste',
-      user: undefined,
+      user,
     };
   }
 }
